4681253410ffe2233406b51a44ff1e804b772b4b
Doom <blacklands00@gmail.com>
2022-05-27 03:01:48 -0300

    Teste brincando com herança

---
+++ Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrecoProduto.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product() { }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual String PriceTag() 
+        {
+           return Name +" $" + Price.ToString("F2", CultureInfo.InvariantCulture);
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using PrecoProduto.Entities;
+
+namespace PrecoProduto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> product = new List<Product>();
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char c = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+                if (c == 'i' || c == 'I')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine());
+                    product.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else if (c == 'u' || c == 'U')
+                {
+                    Console.Write("Manufacture date: (DD/MM/YYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    product.Add(new UsedProduct(name, price, date));
+                }
+                else 
+                {
+                    product.Add(new Product(name,price));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach (Product prod in product)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+        }
+    }
+}
